74898d03fba4a5fd7a78068d9c99f160143866c0
fantasytyx <fantacytyx@foxmail.com>
2025-08-23 16:07:59 +0800

    fix bug - 修复web接口获取不到追番转态的问题 - PGC视频详情页，已追番剧时按钮图标改成红色 - 首页-追番列表，增加筛选操作提示

--- bili-api/src/main/kotlin/dev/aaa1115910/biliapi/repositories/VideoDetailRepository.kt
+++ bili-api/src/main/kotlin/dev/aaa1115910/biliapi/repositories/VideoDetailRepository.kt
@@ -164,6 +164,10 @@ class VideoDetailRepository(
                     seasonId = seasonId,
                     sessData = authRepository.sessionData ?: ""
                 ).getResponseData()
+                webSeasonData.userStatus = BiliHttpApi.getSeasonUserStatus(
+                    seasonId = seasonId!!,
+                    sessData = authRepository.sessionData ?: ""
+                ).getResponseData()
                 val seasonDetail = SeasonDetail.fromSeasonData(webSeasonData)
                 val firstEp = webSeasonData.episodes.firstOrNull() ?: return seasonDetail
 
